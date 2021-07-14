1479e32298048c7d73d5d0c6bad58154c6f439bb
ahmed nagy <a.nagy@nasps.org.eg>
2021-07-14 10:43:53 +0200

    add database old example

--- oldDataBaseExample/app/src/main/java/com/example/roomdatabaseexample/db/entity/Contact.java
+++ oldDataBaseExample/app/src/main/java/com/example/roomdatabaseexample/db/entity/Contact.java
@@ -4,22 +4,23 @@ public class Contact {
 
 
     public static final String TABLE_NAME = "contacts";
-    public static final String COLUMN_ID = "contacts";
-    public static final String COLUMN_NAME = "contacts";
-    public static final String COLUMN_EMAIL = "contacts";
+    public static final String COLUMN_ID = "id";
+    public static final String COLUMN_NAME = "name";
+    public static final String COLUMN_EMAIL = "email";
 
     private String name;
     private String email;
-    private String id;
+    private long id;
 
     public Contact() {
     }
 
-    public Contact(String name, String email, String id) {
+    public Contact(String name, String email, long id) {
         this.name = name;
         this.email = email;
         this.id = id;
     }
+
 
     public String getName() {
         return name;
@@ -37,19 +38,19 @@ public class Contact {
         this.email = email;
     }
 
-    public String getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(String id) {
+    public void setId(int id) {
         this.id = id;
     }
 
     public static final String  CREATE_TABLE =
-            "CREATE TABLE" + TABLE_NAME + "("
-                    + COLUMN_ID + "INTEGER PRIMARY KEY AUTOINCREMENT,"
-                    + COLUMN_NAME + "TEXT,"
-                    + COLUMN_EMAIL + "DATETIME DEFAULT CURRENT_TIMESTAMP"
+            "CREATE TABLE " + TABLE_NAME + "("
+                    + COLUMN_ID + " INTEGER PRIMARY KEY AUTOINCREMENT,"
+                    + COLUMN_NAME + " TEXT,"
+                    + COLUMN_EMAIL + " DATETIME DEFAULT CURRENT_TIMESTAMP"
                     + ") " ;
 
 }
